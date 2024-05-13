a76e4a55e4dea56a5b9df7bd03456520569fe503
marzeverett <marzeverett@gmail.com>
2024-05-13 09:31:17 -0700

    Run 5

--- experiment_parameters/test_run.py
+++ experiment_parameters/test_run.py
@@ -1,14 +1,14 @@
 
 experiments =  {
-    "testing_4": {
+    "testing_6": {
         "index_key": "time_steps",
         "consequent_key": "delta_frost_events",
-        "epochs": 1,
+        "epochs": 10,
         "batch_size": 32, 
         "sequence_limit": 240,
         "sequence_offset": 36,
-        "model_name": "test_model",
+        "model_name": "model_3",
         "verbose": True,
-        "dataset_name": "240_36_delta",
+        "dataset_name": "240_36_delta_constrained",
     },
 }
